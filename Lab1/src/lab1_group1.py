1b8ba7ac4dd34434ec31353c4eba073fda177979
Jayce <jayce.baxter@proton.me>
2025-01-24 10:31:24 -0500

    fixed validation and added venv, just need to actually make the password

--- Lab1/src/lab1_group1.py
+++ Lab1/src/lab1_group1.py
@@ -3,6 +3,10 @@ import string
 
 # Initializing variables
 validation = False
+password_length_valid = False
+no_of_letters_valid = False
+no_of_digits_valid = False
+no_of_symbols_valid = False
 
 # Initializing lists
 letters = []
@@ -14,31 +18,68 @@ random_special = random.choice(string.punctuation)
 
 while validation == False:
 
-    # Prompts the user for password length
-    password_length = input("Please enter the desired length of the password.")
+    # Prompts the user for password length and validates correct input
+    while password_length_valid == False:
 
-    # Prompts the user for number of letters
-    no_of_letters = input("Please enter the number of letters")
+        password_length = input("Please enter the desired length of the password: ")
 
-    for i in no_of_letters:
-        # Generate a random letter
-        random_letter = random.choice(string.ascii_letters)
-        letters.append(random_letter)
+        try:
+            password_length = int(password_length)
+            password_length_valid = True
 
-    # Prompts the user for number of digits
-    no_of_digits = input("Please enter the number of digits")
+        except ValueError:
+            print("Please enter a number")
 
-    for i in no_of_digits:
-        # Generate a random digit
-        random_digit = random.choice(string.digits)
-        digits.append(random_digit)
+    while no_of_letters_valid == False:
 
-# Prompts the user for number of symbols
-no_of_symbols = input("Please enter the number of symbols")
+        no_of_letters = input("Please enter the number of letters: ")
 
-try:
-    no_of_letters = int(no_of_letters)
-    no_of_digits = int(no_of_digits)
-    no_of_symbols = int(no_of_symbols)
+        try:
+            no_of_letters = int(no_of_letters)
+            for i in range(no_of_letters):
+                # Generate a random letter
+                random_letter = random.choice(string.ascii_letters)
+                letters.append(random_letter)
+            no_of_letters_valid = True
 
-except:
+        except ValueError:
+            print("Please enter a number")
+
+
+    while no_of_digits_valid == False:
+
+        # Prompts the user for number of digits
+        no_of_digits = input("Please enter the number of digits: ")
+
+        try:
+            no_of_digits = int(no_of_digits)
+            for i in range(no_of_digits):
+                # Generate a random digit
+                random_digit = random.choice(string.digits)
+                digits.append(random_digit)
+            no_of_digits_valid = True
+
+        except ValueError:
+            print("Please enter a number")
+
+    while no_of_symbols_valid == False:
+        # Prompts the user for number of symbols
+        no_of_symbols = input("Please enter the number of symbols: ")
+
+        try:
+            no_of_symbols = int(no_of_symbols)
+            for i in range(no_of_symbols):
+                # Generate a random special character
+                random_special = random.choice(string.punctuation)
+                symbols.append(random_special)
+            no_of_symbols_valid = True
+
+        except ValueError:
+            print("Please enter a number")
+
+    if password_length_valid == True and no_of_letters_valid == True \
+        and no_of_digits_valid == True and no_of_symbols_valid == True:
+        validation = True
+
+
+print(letters, digits, symbols)
